d25c48e6fceff76fc9d065a8a1012f16869dd6f0
Devin McCabe <devin@sandboxindustries.com>
2012-11-24 14:40:52 -0600

    photo tweaks

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -43,7 +43,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   version :small do
-    process :resize_to_limit => [132, 200]
+    process :resize_to_fit => [999, 200]
+    # process :resize_to_fill => [132, 200]
   end
 
   # version :facebook do
